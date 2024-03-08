2c2008bed92c9dfe2a3a2a851e2e2eeecf93f7e5
Matheus Marti <30555147+Matheus-Marti1@users.noreply.github.com>
2024-03-08 14:47:14 -0300

    Finalizar menu de opções

--- src/view/Main.java
+++ src/view/Main.java
@@ -2,10 +2,33 @@ package view;
 
 import controller.RedesController;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
-    public static void main(String[] args) {
-        RedesController rede = new RedesController();
-        rede.ip();
-    }
+	public static void main(String[] args) {
+		RedesController rede = new RedesController();
+
+		int opc = 0;
+		while (opc != 9) {
+			try {
+				opc = Integer.parseInt(JOptionPane.showInputDialog(
+						"Digite uma opção:\n1 - Mostrar os adaptadores de rede com IPv4\n2 - Mostrar a média do ping para o Google\n9 - Sair"));
+				switch (opc) {
+				case 1:
+					rede.ip();
+					break;
+				case 2:
+					rede.ping();
+					break;
+				case 9:
+					System.exit(0);
+				default:
+					JOptionPane.showMessageDialog(null, "Opção inválida. Digite novamente.");
+				}
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "Opção inválida. Digite novamente.");
+			}
+		}
+	}
 }
