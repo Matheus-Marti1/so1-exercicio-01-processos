5aed06b746932c4a28a152e8224cfcd41172b3e7
Matheus Marti <30555147+Matheus-Marti1@users.noreply.github.com>
2024-03-08 12:52:39 -0300

    Primeiro commit

---
+++ src/controller/RedesController.java
@@ -0,0 +1,59 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class RedesController {
+
+	public RedesController() {
+		super();
+	}
+
+	private String os() {
+		String os = System.getProperty("os.name");
+		return os;
+	}
+
+	@SuppressWarnings("deprecation")
+	public void ip() {
+		String os = os();
+		if (os.contains("Windows")) {
+			try {
+				Process p = Runtime.getRuntime().exec("ipconfig");
+				InputStream fluxo = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				String separador = " ";
+				String nomeAdaptador = "";
+				while (linha != null) {
+					String[] partes = linha.split(separador);
+					if (linha.contains("Adaptador") || linha.contains("Adapter")) {
+						int index = linha.lastIndexOf(":");
+						nomeAdaptador = linha.substring(0, index);
+						linha = buffer.readLine();
+					}
+
+					partes = linha.split(separador);
+					if (linha.contains("IPv4")) {
+						String ipv4 = partes[partes.length - 1];
+						System.out.println(nomeAdaptador);
+						System.out.println("IPv4: " + ipv4);
+						System.out.println();
+					}
+					
+					linha = buffer.readLine();
+				}
+				fluxo.close();
+				leitor.close();
+				buffer.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
